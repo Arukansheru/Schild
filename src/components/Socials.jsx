a633ae23a6fbacd41ff2203b99acc224297f7dd7
Louis Sin <4rc@arcenciel.dev>
2025-12-06 04:33:57 +0800

    doc: Updated GitHub Link

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -4,57 +4,21 @@ function Socials() {
   return (
     <div className="mySocialsComponent">
       <div className="socialLinksContainer">
-        <a
-          href="https://www.linkedin.com/in/louisraphaelsin/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="socialLink"
-        >
-          <img
-            src="icons/LinkedInIcon.svg"
-            alt="linkedIn_Icon"
-            className="socialLinksIcon"
-          />
+        <a href="https://www.linkedin.com/in/louisraphaelsin/" target="_blank" rel="noopener noreferrer" className="socialLink">
+          <img src="icons/LinkedInIcon.svg" alt="linkedIn_Icon" className="socialLinksIcon" />
         </a>
-        <a
-          href="https://github.com/Arc-en-ci3l"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="socialLink"
-        >
-          <img
-            src="icons/GitHubIcon.svg"
-            alt="GitHub_Icon"
-            className="socialLinksIcon"
-          />
+        <a href="https://github.com/Arukansheru" target="_blank" rel="noopener noreferrer" className="socialLink">
+          <img src="icons/GitHubIcon.svg" alt="GitHub_Icon" className="socialLinksIcon" />
         </a>
-        <a
-          href="mailto:4rc@arcenciel.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="socialLink"
-        >
-          <img
-            src="icons/emailIcon.png"
-            alt="Email_Icon"
-            className="socialLinksIcon"
-          />
+        <a href="mailto:4rc@arcenciel.dev" target="_blank" rel="noopener noreferrer" className="socialLink">
+          <img src="icons/emailIcon.png" alt="Email_Icon" className="socialLinksIcon" />
         </a>
       </div>
 
       <button className="downloadResumeBtn">
-        <a
-          href="documents/myResume.pdf"
-          download="myResume.pdf"
-          rel="noopener noreferrer"
-          className="downloadResumeLink"
-        >
+        <a href="documents/myResume.pdf" download="myResume.pdf" rel="noopener noreferrer" className="downloadResumeLink">
           Resume
-          <img
-            src="icons/downloadIcon.svg"
-            alt="Download_Icon"
-            className="downloadIcon"
-          />
+          <img src="icons/downloadIcon.svg" alt="Download_Icon" className="downloadIcon" />
         </a>
       </button>
     </div>
